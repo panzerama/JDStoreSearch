4447f03925e971ca7d815118e7ee46525e767172
Jason Panzer <jason.drew.panzer@gmail.com>
2018-06-19 07:34:54 -0700

    ch 33 pg 1548

---
+++ JDStoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  JDStoreSearch
+//
+//  Created by Jason Panzer on 6/16/18.
+//  Copyright © 2018 Jason Panzer. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
