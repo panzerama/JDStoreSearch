4afc8a0dfd86f25f068d929f331015ec056266b7
Jason Panzer <jason.drew.panzer@gmail.com>
2018-06-19 22:22:50 -0700

    added string encoding

--- JDStoreSearch/SearchViewController.swift
+++ JDStoreSearch/SearchViewController.swift
@@ -41,7 +41,14 @@ class SearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK:- Private Methods
     
+    func iTunesURL(searchText: String) -> URL {
+        let encodedText = searchText.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+        let urlString = String(format: "https://itunes.apple.com/search?term=%@", encodedText)
+        let url = URL(string: urlString)
+        return url!
+    }
 }
 
 extension SearchViewController: UISearchBarDelegate {
@@ -50,18 +57,17 @@ extension SearchViewController: UISearchBarDelegate {
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.resignFirstResponder()
-        searchResults = []
-        if searchBar.text! != "justin bieber" {
-            for i in 0...2 {
-                let searchResult = SearchResult()
-                searchResult.name = String(format: "Fake Result %d for", i)
-                searchResult.artistName = searchBar.text!
-                searchResults.append(searchResult)
-            }
+        if !searchBar.text!.isEmpty {
+            searchBar.resignFirstResponder()
+            
+            hasSearched = true
+            searchResults = []
+            
+            let url = iTunesURL(searchText: searchBar.text!)
+            print("URL: '\(url)'")
+            
+            tableView.reloadData()
         }
-        hasSearched = true
-        tableView.reloadData()
     }
 }
 
